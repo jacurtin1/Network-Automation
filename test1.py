ee2ee48dfad4cbfcbe62cde64a0d5985f8b8b238
John Curtin <jacurtin@gmail.com>
2022-02-24 18:21:47 -0800

    added test files to Githud directory

---
+++ test1.py
@@ -0,0 +1,4 @@
+ip_addr_list = ['192.168.10.10', '192.168.10.11', '192.168.10.15'] 
+
+for ip in ip_addr_list:
+    print('The router management IP is ' + ip)
---
+++ test2.py
@@ -0,0 +1,6 @@
+import string 
+import secrets 
+
+alphabet = string.ascii_letters + string.digits + 
+string.punctuation
+print(alphabet)
